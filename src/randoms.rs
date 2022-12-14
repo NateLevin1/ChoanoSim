0e9569bf0680b29b38ad96ad698a3d7da1e4fe9d
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2022-12-13 21:47:30 -0500

    🔥 Remove seeded random (for now)
    It doesn't help performance much, so more investigation needs to be done.

--- src/randoms.rs
+++ src/randoms.rs
@@ -1,15 +1,22 @@
-use once_cell::unsync::Lazy;
-use rand::{Rng, SeedableRng};
-use rand_xoshiro::Xoshiro256Plus;
+// use once_cell::unsync::Lazy;
+// use rand::{Rng, SeedableRng};
+// use rand_xoshiro::Xoshiro256Plus;
 
 // chosen at random https://xkcd.com/221/
-const RANDOM_SEED: u64 = 230575;
+// const RANDOM_SEED: u64 = 23575;
 
 // To do this quickly, we have to use unsafe. Be careful!
-static mut RANDOM: Lazy<Xoshiro256Plus> = Lazy::new(|| Xoshiro256Plus::seed_from_u64(RANDOM_SEED));
+// static mut RANDOM: Lazy<Xoshiro256Plus> = Lazy::new(|| Xoshiro256Plus::seed_from_u64(RANDOM_SEED));
+
+// pub fn random_float() -> f64 {
+//     unsafe { RANDOM.gen::<f64>() }
+// }
+
+// TODO: this could be improved by using a seeded random like the above.
+//       after doing performance testing, the above doesn't provide a significant benefit, and it loses some scientific accuracy
 
 pub fn random_float() -> f64 {
-    unsafe { RANDOM.gen::<f64>() }
+    js_sys::Math::random()
 }
 
 pub fn random(max: u32) -> u32 {
